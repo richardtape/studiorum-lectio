fad113ef5e67ca4319c90d89c1896a68fbce92b9
Richard Tape <richard.tape@ubc.ca>
2014-09-11 16:25:04 -0700

    ADD submissions can now support custom fields

--- includes/class-studiorum-lectio-post-type.php
+++ includes/class-studiorum-lectio-post-type.php
@@ -101,7 +101,7 @@
 				'has_archive'			=> true,
 				'hierarchical'			=> false,
 				'menu_position'			=> null,
-				'supports'				=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+				'supports'				=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' )
 			);
 
 			register_post_type( $this->slug, $args );
