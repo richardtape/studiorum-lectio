33e42523566aec408189a9038cc5ae3a0bfe8491
Richard Tape <richard@iamfriendly.com>
2014-09-23 15:10:49 -0700

    CHANGE the message for not-member-of-blog slightly

--- includes/templates/user-not-member-of-blog-note.php
+++ includes/templates/user-not-member-of-blog-note.php
@@ -8,7 +8,7 @@
 	 *
 	 */
 	
-	$message = __( 'You are unable to make a submission as, even though you are signed in, you are not a member of this individual site. Please accept the invitation you have been sent or, if you have not received an invitation, please request an invite from your teacher.', 'studiorum-lectio' );
+	$message = __( 'You are unable to make a submission as, even though you are signed in, you are not a member of this individual site. Please accept the invitation you have been sent via email or, if you have not received an invitation, please request an invite from your instructor.', 'studiorum-lectio' );
 	$message = apply_filters( 'studiorum_lectio_user_not_member_of_blog_note', $message );
 
 ?>
