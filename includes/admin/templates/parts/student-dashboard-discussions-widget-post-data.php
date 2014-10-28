9bf874a446d30ab1594eec10c5ef8fed47642159
Richard Tape <richard@iamfriendly.com>
2014-10-28 13:16:35 -0700

    ADD author name to output for group submissions

--- includes/admin/templates/parts/student-dashboard-discussions-widget-post-data.php
+++ includes/admin/templates/parts/student-dashboard-discussions-widget-post-data.php
@@ -8,13 +8,18 @@
 	 *
 	 */
 
+	// Get the author details
+	$postID = $postInfo['postID'];
+	$authorID = get_post_field( 'post_author', $postID );
+	$author = ( get_the_author_meta( 'user_nicename', $authorID ) != '' ) ? get_the_author_meta( 'user_nicename', $authorID ) : get_the_author_meta( 'user_login', $authorID );
+
 ?>
 
 	<li>
 
 		<p class="details">
 			<?php _e( 'Submission made on ', 'studiorum-lectio' ); ?> <?php echo $postInfo['date']; ?>.
-			<a href="<?php echo esc_url( $postInfo['permalink'] ); ?>" title="" target="_BLANK"><?php _e( 'View Submission', 'studiorum-lectio' ); ?></a>
+			<a href="<?php echo esc_url( $postInfo['permalink'] ); ?>" title="" target="_BLANK"><?php _e( 'View Submission', 'studiorum-lectio' ); ?></a> <?php _e( 'by', 'studiorum-lectio' ); ?> <?php echo $author; ?>
 		</p>
 
 	</li>
