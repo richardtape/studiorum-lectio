a5cd63d7d31a751c961cd6273bdf9cc1c235a8e3
Richard Tape <richard@iamfriendly.com>
2014-10-28 13:17:29 -0700

    CHANGE allow some html in the 'this is one of your submissions' message to allow a link to the print stylesheet

--- includes/templates/author-notice-above-submission.php
+++ includes/templates/author-notice-above-submission.php
@@ -11,10 +11,20 @@
 	$message = __( 'This is one of your submissions. It is visible to you and your instructor(s).', 'studiorum-lectio' );
 	$message = apply_filters( 'studiorum_lectio_author_note_above_submission', $message );
 
+	$allowedHTML = apply_filters( 'studiorum_lectio_author_note_above_submission_allowed_html', array(
+		'a' => array(
+			'href' => array(),
+			'title' => array()
+		),
+		'br' => array(),
+		'em' => array(),
+		'strong' => array(),
+	) );
+
 ?>
 
 	<div class="notice dark author-submission-notice">
 
-		<p><?php echo esc_html( $message ); ?></p>
+		<p><?php echo wp_kses( $message ); ?></p>
 
 	</div><!-- .author-submission-notice -->
